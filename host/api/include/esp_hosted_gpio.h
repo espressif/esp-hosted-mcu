97192952ab44370030adb21dfc0e1c15f6329e36
YoussefTarkhan <tarkhan@astera-led.com>
2025-08-28 10:22:16 +0200

    host/api: add esp_hosted_gpio.h

---
+++ host/api/include/esp_hosted_gpio.h
@@ -0,0 +1,26 @@
+/* APIs to do control GPIOs of the co-processor
+ *
+ * Note: This API is platform dependent
+ *
+ * Source for the API should be in host/port/<platform>/...
+ *
+ */
+
+#ifndef __ESP_HOSTED_GPIO_H__
+#define __ESP_HOSTED_GPIO_H__
+
+#ifdef ESP_PLATFORM
+// OTA API for ESP-IDF
+#include "esp_err.h"
+
+esp_err_t esp_hosted_gpio_config(const gpio_config_t *pGPIOConfig);
+esp_err_t esp_hosted_gpio_reset_pin(gpio_num_t gpio_num);
+esp_err_t esp_hosted_gpio_set_level(gpio_num_t gpio_num, uint32_t level);
+int esp_hosted_gpio_get_level(gpio_num_t gpio_num);
+esp_err_t esp_hosted_gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t mode);
+esp_err_t esp_hosted_gpio_input_enable(gpio_num_t gpio_num);
+esp_err_t esp_hosted_gpio_set_pull_mode(gpio_num_t gpio_num, gpio_pull_mode_t pull);
+
+#endif
+
+#endif /*__ESP_HOSTED_OTA_H__*/
