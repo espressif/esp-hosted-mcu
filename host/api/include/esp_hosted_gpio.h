b8c821dc07529fd0f812cd874c199e51c2c3310f
YoussefTarkhan <tarkhan@astera-led.com>
2025-09-02 10:47:38 +0200

    removed #ifdef ESP_PLATFORM

--- host/api/include/esp_hosted_gpio.h
+++ host/api/include/esp_hosted_gpio.h
@@ -9,8 +9,7 @@
 #ifndef __ESP_HOSTED_GPIO_H__
 #define __ESP_HOSTED_GPIO_H__
 
-#ifdef ESP_PLATFORM
-// OTA API for ESP-IDF
+
 #include "esp_err.h"
 
 esp_err_t esp_hosted_gpio_config(const gpio_config_t *pGPIOConfig);
@@ -21,6 +20,6 @@ esp_err_t esp_hosted_gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t mode);
 esp_err_t esp_hosted_gpio_input_enable(gpio_num_t gpio_num);
 esp_err_t esp_hosted_gpio_set_pull_mode(gpio_num_t gpio_num, gpio_pull_mode_t pull);
 
-#endif
+
 
 #endif /*__ESP_HOSTED_OTA_H__*/
