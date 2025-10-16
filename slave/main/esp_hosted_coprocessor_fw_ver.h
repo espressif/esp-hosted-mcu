5e402a525d67883861aa4d29895a448eae0a4f93
Yogesh Mantri <yogesh.mantri@espressif.com>
2025-10-16 22:27:56 +0800

    Minor fixes in slave ota example

--- slave/main/esp_hosted_coprocessor_fw_ver.h
+++ slave/main/esp_hosted_coprocessor_fw_ver.h
@@ -7,7 +7,8 @@
 #define __ESP_HOSTED_COPROCESSOR_FW_VER_H__
 #define PROJECT_VERSION_MAJOR_1 2
 #define PROJECT_VERSION_MINOR_1 6
-#define PROJECT_VERSION_PATCH_1 0
+#define PROJECT_VERSION_PATCH_1 1
 
 #define ESP_HOSTED_VERSION_VAL(major, minor, patch) ((major << 16) | (minor << 8) | (patch))
+
 #endif
