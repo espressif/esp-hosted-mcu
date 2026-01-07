c26d138384b4c3642aebb5ff57ddadbeefbb742f
Yogesh Mantri <yogesh.mantri@espressif.com>
2026-01-08 01:07:59 +0800

    fix(slave_ota): remove forced QIO flash mode to fix slave OTA failures
    Back-to-back OTA updates on the slave were failing due to
    CONFIG_ESPTOOLPY_FLASHMODE_QIO being enforced.
    This change removes the forced flash mode.
    
    Fixes https://github.com/espressif/esp-hosted-mcu/issues/143
    
    Fixes https://github.com/espressif/esp-hosted-mcu/issues/113

--- host/esp_hosted_host_fw_ver.h
+++ host/esp_hosted_host_fw_ver.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2025-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -14,8 +14,8 @@
 #define __ESP_HOSTED_HOST_FW_VERSION_H__
 
 #define ESP_HOSTED_VERSION_MAJOR_1 2
-#define ESP_HOSTED_VERSION_MINOR_1 8
-#define ESP_HOSTED_VERSION_PATCH_1 5
+#define ESP_HOSTED_VERSION_MINOR_1 9
+#define ESP_HOSTED_VERSION_PATCH_1 0
 
 /**
  * Macro to convert version number into an integer
