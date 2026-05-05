f428785dc8ab481503083da27d461ee9e7171698
Felix <fuxing319@gmail.com>
2026-05-05 16:23:17 +0800

    core(rpc_wrap): align rpc_wrap.h with h_wifi_* types, add adapters at ctrl_cmd_t boundaries
    - rpc_wrap.h: replace native wifi_* parameter types with h_wifi_* types
      to match h_rpc_wrap.c implementation signatures after typedef removal
    - h_rpc_wrap.c: replace direct memcpy of h_wifi_* into ctrl_cmd_t with
      explicit native struct conversion via h_wifi_type_adapt functions
    - Replace native WIFI_* constants with H_WIFI_* in internal logic
    - Convert back to native before ctrl_cmd_t field assignment
    
    门槛 2 WP 2.3 / 2.5

--- host/drivers/rpc/wrap/rpc_wrap.h
+++ host/drivers/rpc/wrap/rpc_wrap.h
@@ -13,7 +13,7 @@ extern "C" {
 #endif
 
 /** Includes **/
-#include "esp_wifi.h"
+#include "h_wifi_types.h"
 #include "port_esp_hosted_host_wifi_config.h"
 #include "port_esp_hosted_host_config.h"
 #include "port_esp_hosted_host_openthread.h"
@@ -49,53 +49,53 @@ esp_err_t rpc_deinit(void);
 esp_err_t rpc_unregister_event_callbacks(void);
 esp_err_t rpc_register_event_callbacks(void);
 
-esp_err_t rpc_wifi_init(const wifi_init_config_t *arg);
+esp_err_t rpc_wifi_init(const h_wifi_init_config_t *arg);
 esp_err_t rpc_wifi_deinit(void);
-esp_err_t rpc_wifi_set_mode(wifi_mode_t mode);
-esp_err_t rpc_wifi_get_mode(wifi_mode_t* mode);
+esp_err_t rpc_wifi_set_mode(h_wifi_mode_t mode);
+esp_err_t rpc_wifi_get_mode(h_wifi_mode_t* mode);
 esp_err_t rpc_wifi_start(void);
 esp_err_t rpc_wifi_stop(void);
 esp_err_t rpc_wifi_connect(void);
 esp_err_t rpc_wifi_disconnect(void);
-esp_err_t rpc_wifi_set_config(wifi_interface_t interface, wifi_config_t *conf);
-esp_err_t rpc_wifi_get_config(wifi_interface_t interface, wifi_config_t *conf);
-esp_err_t rpc_wifi_get_mac(wifi_interface_t mode, uint8_t mac[6]);
-esp_err_t rpc_wifi_set_mac(wifi_interface_t mode, const uint8_t mac[6]);
+esp_err_t rpc_wifi_set_config(h_wifi_interface_t interface, h_wifi_config_t *conf);
+esp_err_t rpc_wifi_get_config(h_wifi_interface_t interface, h_wifi_config_t *conf);
+esp_err_t rpc_wifi_get_mac(h_wifi_interface_t mode, uint8_t mac[6]);
+esp_err_t rpc_wifi_set_mac(h_wifi_interface_t mode, const uint8_t mac[6]);
 esp_err_t rpc_wifi_set_scan_parameters(const wifi_scan_default_params_t *config);
 esp_err_t rpc_wifi_get_scan_parameters(wifi_scan_default_params_t *config);
 
-esp_err_t rpc_wifi_scan_start(const wifi_scan_config_t *config, bool block);
+esp_err_t rpc_wifi_scan_start(const h_wifi_scan_config_t *config, bool block);
 esp_err_t rpc_wifi_scan_stop(void);
 esp_err_t rpc_wifi_scan_get_ap_num(uint16_t *number);
-esp_err_t rpc_wifi_scan_get_ap_record(wifi_ap_record_t *ap_record);
-esp_err_t rpc_wifi_scan_get_ap_records(uint16_t *number, wifi_ap_record_t *ap_records);
+esp_err_t rpc_wifi_scan_get_ap_record(h_wifi_ap_record_t *ap_record);
+esp_err_t rpc_wifi_scan_get_ap_records(uint16_t *number, h_wifi_ap_record_t *ap_records);
 esp_err_t rpc_wifi_clear_ap_list(void);
 esp_err_t rpc_wifi_restore(void);
 esp_err_t rpc_wifi_clear_fast_connect(void);
 esp_err_t rpc_wifi_deauth_sta(uint16_t aid);
-esp_err_t rpc_wifi_sta_get_ap_info(wifi_ap_record_t *ap_info);
-esp_err_t rpc_wifi_set_ps(wifi_ps_type_t type);
-esp_err_t rpc_wifi_get_ps(wifi_ps_type_t *type);
+esp_err_t rpc_wifi_sta_get_ap_info(h_wifi_ap_record_t *ap_info);
+esp_err_t rpc_wifi_set_ps(h_wifi_ps_type_t type);
+esp_err_t rpc_wifi_get_ps(h_wifi_ps_type_t *type);
 esp_err_t rpc_wifi_set_storage(wifi_storage_t storage);
-esp_err_t rpc_wifi_set_bandwidth(wifi_interface_t ifx, wifi_bandwidth_t bw);
-esp_err_t rpc_wifi_get_bandwidth(wifi_interface_t ifx, wifi_bandwidth_t *bw);
+esp_err_t rpc_wifi_set_bandwidth(h_wifi_interface_t ifx, h_wifi_bandwidth_t bw);
+esp_err_t rpc_wifi_get_bandwidth(h_wifi_interface_t ifx, h_wifi_bandwidth_t *bw);
 esp_err_t rpc_wifi_set_channel(uint8_t primary, wifi_second_chan_t second);
 esp_err_t rpc_wifi_get_channel(uint8_t *primary, wifi_second_chan_t *second);
 esp_err_t rpc_wifi_set_country_code(const char *country, bool ieee80211d_enabled);
 esp_err_t rpc_wifi_get_country_code(char *country);
-esp_err_t rpc_wifi_set_country(const wifi_country_t *country);
-esp_err_t rpc_wifi_get_country(wifi_country_t *country);
-esp_err_t rpc_wifi_ap_get_sta_list(wifi_sta_list_t *sta);
+esp_err_t rpc_wifi_set_country(const h_wifi_country_t *country);
+esp_err_t rpc_wifi_get_country(h_wifi_country_t *country);
+esp_err_t rpc_wifi_ap_get_sta_list(h_wifi_sta_list_t *sta);
 esp_err_t rpc_wifi_ap_get_sta_aid(const uint8_t mac[6], uint16_t *aid);
 esp_err_t rpc_wifi_sta_get_rssi(int *rssi);
-esp_err_t rpc_wifi_set_protocol(wifi_interface_t ifx, uint8_t protocol_bitmap);
-esp_err_t rpc_wifi_get_protocol(wifi_interface_t ifx, uint8_t *protocol_bitmap);
+esp_err_t rpc_wifi_set_protocol(h_wifi_interface_t ifx, uint8_t protocol_bitmap);
+esp_err_t rpc_wifi_get_protocol(h_wifi_interface_t ifx, uint8_t *protocol_bitmap);
 esp_err_t rpc_wifi_set_max_tx_power(int8_t power);
 esp_err_t rpc_wifi_get_max_tx_power(int8_t *power);
 esp_err_t rpc_wifi_sta_get_negotiated_phymode(wifi_phy_mode_t *phymode);
 esp_err_t rpc_wifi_sta_get_aid(uint16_t *aid);
-esp_err_t rpc_wifi_set_inactive_time(wifi_interface_t ifx, uint16_t sec);
-esp_err_t rpc_wifi_get_inactive_time(wifi_interface_t ifx, uint16_t *sec);
+esp_err_t rpc_wifi_set_inactive_time(h_wifi_interface_t ifx, uint16_t sec);
+esp_err_t rpc_wifi_get_inactive_time(h_wifi_interface_t ifx, uint16_t *sec);
 esp_err_t rpc_get_coprocessor_fwversion(esp_hosted_coprocessor_fwver_t *ver_info);
 esp_err_t rpc_get_cp_info(uint32_t *cp_chip_id, char *cp_target_name, size_t cp_target_name_len);
 
