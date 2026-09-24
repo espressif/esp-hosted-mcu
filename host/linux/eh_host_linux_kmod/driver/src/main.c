f51c70f25c47271ba11b748c0784b0d357807845
Yogesh Mantri <yogesh.mantri@espressif.com>
2026-09-25 02:06:13 +0800

    release: 3.0.8

--- host/linux/eh_host_linux_kmod/driver/src/main.c
+++ host/linux/eh_host_linux_kmod/driver/src/main.c
@@ -86,7 +86,7 @@ MODULE_AUTHOR("Amey Inamdar <amey.inamdar@espressif.com>");
 MODULE_AUTHOR("Mangesh Malusare <mangesh.malusare@espressif.com>");
 MODULE_AUTHOR("Yogesh Mantri <yogesh.mantri@espressif.com>");
 MODULE_DESCRIPTION("Host driver for ESP-Hosted solution");
-MODULE_VERSION("3.0.6");
+MODULE_VERSION("3.0.8");
 
 module_param(resetpin, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(resetpin, "Host's GPIO pin number which is connected to ESP32's EN to reset ESP32 device");
